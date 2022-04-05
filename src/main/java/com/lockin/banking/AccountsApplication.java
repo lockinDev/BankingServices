6fd5d5c2f28fa665db8ab89f33312795ef1dc4bf
Javier <javieralmario12@gmail.com>
2022-04-04 21:56:09 -0400

    Added boot class and model.

---
+++ src/main/java/com/lockin/banking/AccountsApplication.java
@@ -0,0 +1,16 @@
+package com.lockin.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+}
