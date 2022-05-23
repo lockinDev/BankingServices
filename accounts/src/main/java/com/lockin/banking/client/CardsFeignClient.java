0a315dd631f413cbd1471b147f3f9a0f2a41a7ec
Javier <javieralmario12@gmail.com>
2022-05-23 17:28:19 -0400

    MOdified servies to integrate discovery service. Added feign funcionality to request other services each other.

---
+++ accounts/src/main/java/com/lockin/banking/client/CardsFeignClient.java
@@ -0,0 +1,20 @@
+package com.lockin.banking.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lockin.banking.model.Cards;
+import com.lockin.banking.model.Customer;
+
+
+
+@FeignClient("cards")
+public interface CardsFeignClient {
+
+	@RequestMapping(method = RequestMethod.POST, value = "myCards", consumes = "application/json")
+	List<Cards> getCardDetails(@RequestBody Customer customer);
+}
--- accounts/src/main/java/com/lockin/banking/controller/AccountsController.java
+++ accounts/src/main/java/com/lockin/banking/controller/AccountsController.java
@@ -3,13 +3,20 @@
  */
 package com.lockin.banking.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lockin.banking.client.CardsFeignClient;
+import com.lockin.banking.client.LoansFeignClient;
 import com.lockin.banking.model.Accounts;
+import com.lockin.banking.model.Cards;
 import com.lockin.banking.model.Customer;
+import com.lockin.banking.model.CustomerDetails;
+import com.lockin.banking.model.Loans;
 import com.lockin.banking.repository.AccountsRepository;
 
 
@@ -20,6 +27,12 @@ public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
+	
+	@Autowired
+	LoansFeignClient loansFeignClient;
+
+	@Autowired
+	CardsFeignClient cardsFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -30,6 +43,22 @@ public class AccountsController {
 		} else {
 			return null;
 		}
+
+	}
+	
+	
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+		
+		return customerDetails;
 
 	}
 
